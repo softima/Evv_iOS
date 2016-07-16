108a657e81431a01a5c560978bde8bc498f3d66f
softima <softima.inc@gmail.com>
2016-07-16 05:39:04 -0500

    Update check_in_summary.js

--- js/check_in_summary.js
+++ js/check_in_summary.js
@@ -111,7 +111,7 @@ function fail(uuid)
 		});
               },
               success: function (data) {
-            	  $("#pageLoader").hide(); 
+            	             	  $("#pageLoader").hide(); 
             	  $('body').attr('id', '');
             	  $("#form_id").show(); 
             	  $("#link_id").show(); 
@@ -123,37 +123,26 @@ function fail(uuid)
  check_in_timestamp=data.checkin_data[0].check_in_timestamp;
  server_time=data.server_time;
  
+// alert(" ajax request server_time"+server_time);
+ //alert("ajax request checked_in_duration"+checked_in_duration);
  
  
  
  
- /*alert("Custom Alerts");
- alert("Check in timestamp"+check_in_timestamp);
- alert("Server time"+server_time);
- */
-
  
  
- /* 
- var diff_times=check_in_timestamp.getDate()-server_time.getDate();
- if(diff_times>10)
-	 {
-	 alert("Greater than 10");
-	 }
  
- */
  
  
  $("#exampleInputpatientname1").html(patient_name);
  $("#exampleInputvisittype1").html(visit_type);
  $("#exampleInputscheduledtime1").html(scheduled_time);
  $("#checked_in_time_temp").html(checked_in_time);
-// alert("checked in"+checked_in_duration);
+ //alert("checked in"+checked_in_duration);
  $("#checked_in_time_dur").html(checked_in_duration);
  $("#checked_in_time_dur").val(checked_in_duration);
  $("#check_in_timestamp").val(check_in_timestamp);
  $("#server_time").val(server_time);
- 
  
  
  
@@ -164,30 +153,40 @@ function fail(uuid)
  var scheduled_time = json.checkin_data[0].sch_start_timestamp+' - '+json.patientdata[0].sch_end_timestamp;
 */
   //$("#checked_in_time").val(json.checkin_data[0].checked_in_time);
-	
- 
-  	devicesec=deviceMobileTime();
- 
- var  server_time= $('#server_time').val();
- 
-// alert("Devicesec"+devicesec);
-// alert("server time"+server_time);
- 
- var devicesec=hmsToSecondsOnly(devicesec);
- 
+
+  var  server_time= $('#server_time').val();
  var server_time=hmsToSecondsOnly(server_time);
  
-			// alert("devicesec after converting"+devicesec);
-			// alert("server_time after converting"+server_time);
-			 
- if(devicesec-server_time>180 || devicesec-server_time<0)
+ //alert("server_time in seconds"+server_time);
+ 
+ 
+ devicesec=deviceMobileTime();
+ 
+ //alert("devicesec normal time "+devicesec);
+
+ 
+ 
+ var devicesec=hmsToSecondsOnly(devicesec);
+  //alert("devicesec in seconds"+devicesec);
+ 
+ diff=devicesec-server_time;
+ //alert("diff"+diff);
+ //alert("datatype");
+ datatypedd=typeof diff;
+ //alert("datatype"+datatypedd);
+ 
+ 
+ if(devicesec-server_time > 1800 || devicesec-server_time < -2)
 	{
+	//alert("inside condition");
 	  $('#hidden_timecheck').val('1');
 	   $('#duration_time_temp').html('Improper Time');
 	   $('#time1').html('Improper Time'); 
 	$("#proceed_to_check_out").prop('disabled', true);
 		  
-	}  
+	}
+ 
+  	  
 }
 
 });
@@ -196,7 +195,8 @@ function fail(uuid)
 		});
 		}
 		}
-		window.onload = function(){date()}, setInterval(function(){date()}, 1000);
+    
+		window.onload = function(){}, setInterval(function(){date()}, 1000);
 
 
 		 function onBackKeyDown(e) {
@@ -239,17 +239,24 @@ function fail(uuid)
 		 }
 		
 		function date() {
-			
 
-			 var devicesec=deviceMobileTime();
+			
 			 
 			 var  server_time= $('#server_time').val();
+			 //alert("inside  date"+server_time);
+			  var server_time=hmsToSecondsOnly(server_time);
+			  //alert("inside  date in sec"+server_time);
+			 
+			 var devicesec=deviceMobileTime();
+			  //alert("inside  date devicesec"+devicesec);
+			
 			 
 			 var devicesec=hmsToSecondsOnly(devicesec);
+			 //alert("inside  date devicesec seconds"+devicesec);
+			
 			 
-			 var server_time=hmsToSecondsOnly(server_time);
 			 
-			 if(devicesec-server_time>180 || devicesec-server_time<0)
+			 if( devicesec-server_time==-6.666)
 				{
 				 
 				  $('#hidden_timecheck').val('1');
@@ -266,13 +273,9 @@ function fail(uuid)
 					
 					
 					//$("#proceed_to_check_out").prop('disabled', false);
-			
-			
-			
     var time = new Date();
     var hours = addZero(time.getHours());
     var minutes = addZero(time.getMinutes());
-   
     var current_time = hours + ":" + minutes;
    
     //now = now.getHours()+':'+now.getMinutes()+':'+now.getSeconds();
@@ -282,33 +285,63 @@ function fail(uuid)
    var  checked_in_time_temp= $('#checked_in_time_dur').val();
     if(checked_in_time_temp!='')
      {
+    	
+        var checked_in_time_temp = new Date(checked_in_time_temp);;
+        var curr_date_checked_in_time_temp = checked_in_time_temp.getDate();
+        var curr_month_checked_in_time_temp = checked_in_time_temp.getMonth()+1;
+        var curr_year_checked_in_time_temp = checked_in_time_temp.getFullYear();
+        var d1 = '' + curr_year_checked_in_time_temp  + ' ' + curr_month_checked_in_time_temp + ' ' + curr_date_checked_in_time_temp+ ' ' +checked_in_time_temp.getHours() + ':' +checked_in_time_temp.getMinutes() ;
+        var d1 = new Date(d1);
+    	
+    	
+   	
+            
+  // Do your operations
+   
+  //End Time
+  var d2 = new Date();
+  
+  var mydate = new Date();
+  var curr_date = mydate.getDate();
+  var curr_month = mydate.getMonth()+1;
+  var curr_year = mydate.getFullYear();
 
-	
-var d1 = moment(checked_in_time_temp,"YYYY-MM-DD HH:mm");
-var d3 = moment();
- var a=d3.diff(d1);
+  var mydatestr = '' + curr_year  + ' ' +
+  curr_month + ' ' + 
+  curr_date+ ' ' +
+  mydate.getHours() + ':' +
+  mydate.getMinutes() ;
+var d3 = new Date(mydatestr);
 
+  
+  
+  //x=d1.getTime() - d2.getTime();
+  // x=d2.getTime() - d1.getTime();
+   x=d3.getTime() - d1.getTime();
+
+   
   //Time difference in milli seconds
  // document.write("Your Operation took  " + (d2.getTime() - d1.getTime()) + " milliseconds");
-   checkintimediff=msToTime(a);
-   
+   checkintimediff=msToTime(x);
+	   
    $('#duration_time_temp').html(checkintimediff);
+   
    var  check_in_timestamp= $('#check_in_timestamp').val();
-   //alert("check in timestamp"+ check_in_timestamp);
+  // alert("check in timestamp"+ check_in_timestamp);
   var zone=timezoneDetect(check_in_timestamp);
   //alert("zone detect"+zone);
    if(zone=='false')
     {
 	   
 	   $('#duration_time_temp').html('Improper Timezone');
+	   $('#time1').html('Improper Time'); 
+	$("#proceed_to_check_out").prop('disabled', true);
     }
-  
+ 
      }
-	 }
-	 }
-	 
-	 
+				}
    
+}
 }
 		function addZero(i) {
 		    if (i < 10) {
@@ -479,11 +512,10 @@ document.getElementById("diff").value = diff(start, end);
 function exitFromApp()
 {
 	$(".proceed_to_check_out_button").prop('disabled', true);
-
 	$(".show_schedules_button").prop('disabled', true);
 	$(".exit_app").prop('disabled', true);
 
-    window.location='./logout.html';
+     navigator.app.exitApp();
 }
 
 
@@ -491,13 +523,10 @@ function exitFromApp()
 
 function goToCheckOut()
 {
-	
-	//var sch_uuid = getURLParameters('sch_uuid');
-	
-	//window.location.href="check_out.html?sch_uuid="+sch_uuid;
 	$(".proceed_to_check_out_button").prop('disabled', true);
 	 $('#hidden_timecheck').val('1');
-	  //document.getElementsByClassName("show_schedules_button").disabled = true;
+
+	  document.getElementsByClassName("show_schedules_button").disabled = true;
 	
 	$(".show_schedules_button").prop('disabled', true);
 	$(".exit_app").prop('disabled', true);
@@ -518,50 +547,13 @@ var sch_uuid = getURLParameters('sch_uuid');
 
 }
 function ShowError(error) {
-	
-		 bootbox.dialog({
-	closeButton: false,
-	  message: "Location is not enabled. Kindly turn it on by going to Settings-->Privacy-->Location Services-->On.",
-	  title: "Need access to Location",
-	  buttons: {
-	    success: {
-	      label: "Settings",
-	      className: "btn-primary",
-	      callback: function() {
-	    	   cordova.plugins.diagnostic.switchToSettings(successCallback, errorCallback);
-	      }
-	    },
-	    danger: {
-	      label: "Cancel",
-	      className: "btn-warning",
-	      callback: function() {
-	        
-	      }
-	    }
-	    
-	  }
-	});
-		 
-	
-	
 	$(".proceed_to_check_out_button").prop('disabled', false);
 	$(".show_schedules_button").prop('disabled', false);
 	$(".exit_app").prop('disabled', false);
 	 
-//window.MainActivity.showSettingsAlert();
+window.MainActivity.showSettingsAlert();
 
 }
-
- function successCallback()
-		 {
-		 
-		 }
-		 
-		 function errorCallback()
-		 {
-		 
-		 }
-
 
 function go_to_show_schedules()
 {
